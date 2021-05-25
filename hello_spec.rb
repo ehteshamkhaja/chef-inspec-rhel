873743cfbd6e9a16035eb431da47f66789f203d6
root <root@controlnode.example.com>
2021-05-25 01:54:23 -0700

    test inspec

---
+++ hello_spec.rb
@@ -0,0 +1,15 @@
+control "world-1.0" do
+  impact 1.0
+  title "Hello World"
+  desc "Text should include words Hello world"
+  describe file('hello.txt') do 
+    its('content') { should match 'Hello World'}
+  end
+end
+
+describe file('/etc/sysconfig/network-scripts/ifcfg-eth0') do 
+  its(:content) { should match /TYPE=Ethernet/ }
+  its(:content) { should match /BOOTPROTO=static/ }
+  its(:content) { should match /ONBOOT=yes/ }
+  its(:content) { should match /NM_CONTROLLED=no/ }
+end
