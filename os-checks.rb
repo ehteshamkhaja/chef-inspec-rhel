2eccfbcd360049d566489dab68bd590dbc6e6da0
ehteshamkhaja <ehteshamkhaja@gmail.com>
2021-07-20 15:21:44 +0530

    Update os-checks.rb
    added control block

--- os-checks.rb
+++ os-checks.rb
@@ -1,11 +1,27 @@
+control 'Check CPU' do
+  impact 1.0                               
+  title 'Platform Common Checks'
+  desc 'BASE Cecks'
+
 describe command('grep -i processor /proc/cpuinfo | cut -f2 -d :') do
    its('stdout') { should match (/[0]/) }
+ end
 end
 
+control 'Check Memory' do
+  impact 1.0                               
+  title 'Platform Common Checks'
+  desc 'BASE Cecks'
 describe command('grep -i MemTotal /proc/meminfo | cut -f2 -d :') do
    its('stdout') { should match (/[667984]/) }
 end
+end
 
+control 'Check OS Version' do
+  impact 1.0                               
+  title 'Platform Common Checks'
+  desc 'BASE Cecks'
 describe command('grep -i PRETTY_NAME /etc/os-release | cut -f2 -d =') do
    its('stdout') { should match (/["CentOS Linux 7 (Core)"]/) }
+end
 end
